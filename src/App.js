44cdc2526be6fb12c07cfc4418dd8bf6976802f4
Michael Camp <51518906+MichaelCampAZ@users.noreply.github.com>
2022-02-17 16:20:58 -0700

    Map center set to AZ

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <MapContainer center={[51.505, -0.09]} zoom={13}>
+      <MapContainer center={[34.185, -111.476]} zoom={7}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>    </div>
+
+      </MapContainer>
+    </div>
   );
 }
 
