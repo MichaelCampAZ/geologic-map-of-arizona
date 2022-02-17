0e8cc531efa7fca095a63c1c56622ef04abf59fe
Michael Camp <51518906+MichaelCampAZ@users.noreply.github.com>
2022-02-17 16:46:53 -0700

    Added map container settings
    Used the same values as the last version of the project. Will adjust if needed.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <MapContainer center={[34.185, -111.476]} zoom={7}>
+      <MapContainer center={[34.1618, -111.6211]} maxBounds={[[29.96818929679422,-126.13671875],[38.34395908944491,-97.226318359375]]} zoom={7} minZoom={5} >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
